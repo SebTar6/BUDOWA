8e906e59eb90ca02c0c27dbf41dcbe7304329497
KXORATH <minecraftKXORATH@gmail.com>
2021-06-12 02:29:19 +0200

    koniec na dzis

--- docs/js/main.js
+++ docs/js/main.js
@@ -42,8 +42,48 @@ function dodajZamowienie() {
     item.email = document.getElementById('email').value;
     item.produkt = document.getElementById('produkt').value;
     item.liczba = document.getElementById('liczba').value;
+    item.data = new Date();
     var lista = JSON.parse(localStorage.getItem('lista'));
     if (lista === null) lista = [];
     lista.push(item);
     localStorage.setItem('lista', JSON.stringify(lista));
+}
+
+function pokazListe() {
+    var lista = JSON.parse(localStorage.getItem('lista'));
+    var el = document.getElementById('zamowienia');
+    var str = "<hr><h1>Twoja lista zamówień: </h1>";
+    if (lista === null) el.innerHTML = str + "<p>Pusta lista zamówień</p>";
+    else {
+        for (i = 0; i < lista.length; i++) {
+            str += "<h3>Zamówienie z dnia " + lista[i].data + ":</h3>";
+            str += "Imię i nazwisko: " + lista[i].imie + "<br>";
+            str += "Ulica: " + lista[i].ulica + "<br>";
+            str += "Kod pocztowy: " + lista[i].kod + "<br>";
+            str += "Miasto: " + lista[i].miasto + "<br>";
+            str += "Telefon: " + lista[i].telefon + "<br>";
+            str += "Email: " + lista[i].email + "<br>";
+            str += "Produkt: " + lista[i].produkt + "<br>";
+            str += "Liczba sztuk: " + lista[i].liczba + "<br>";
+            str += "<button class='' type='button' onclick='usunZamowienie(" + i + ")' >Usuń</button>";
+            str += "<button class='' type='button' onclick='edycja(" + i + ")'>Edytuj</button>";
+            str += "<br><hr style='width: 40%;'>";
+        }
+    }
+    el.innerHTML = str;
+}
+
+function usunListe() {
+    if (confirm("Usunąć listę zamówień?")) {
+        localStorage.removeItem('lista');
+        //zaktualizuj widok na stronie
+        $('#zamowienia').html('');
+    }
+}
+
+function usunZamowienie(i) {
+    var lista = JSON.parse(localStorage.getItem('lista'));
+    if (confirm("Usunąć zamówienie?")) lista.splice(i, 1);
+    localStorage.setItem('lista', JSON.stringify(lista));
+    pokazListe();
 }
